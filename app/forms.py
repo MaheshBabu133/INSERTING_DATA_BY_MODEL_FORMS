2339d0b165bb1c2ced3ff924d9f691107ad84526
MaheshBabu133 <maheshbabuntr123@gmail.com>
2023-04-24 14:37:47 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -8,6 +8,19 @@ class WebpageForm(forms.ModelForm):
     class Meta:
         model=Webpage
         fields='__all__'
+        #fields=['name','url']     #it will display only name and url  at front end
+        #fields=['name']           #it will display only name 
+        #exclude=['name','url']    #it will display except name and url  at front end
+        #exclude=['name']          #it will display except name   at front end
+
+        #labels={'topic_name':'TN','name':'N','email':'mail_id'} #it will replace the previous label name with new label name
+        #widgets={'url':forms.PasswordInput,'name':forms.Textarea} 
+        #above widget make url field as password and name is as like as text area
+        #help_texts={'topic_name':'should not be interger','name':'Alphabets only ','email':'must ends with the @gmail.com/in/org ','url':'it must starts with http://'}
+        #above help_texts are used to provide additional content at the end of the input element
+        #It helps to user understanding
+
+
 class AccessRecordForm(forms.ModelForm):
     class Meta():
         model=AccessRecord
